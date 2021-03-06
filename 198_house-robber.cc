be7535fdcf3c51d48b89db01af19e39556e024a7
Moon Dongyoung <scintillavoy@gmail.com>
2021-03-06 14:47:27 +0900

    Solve 198_house-robber.cc

---
+++ 198_house-robber.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        } else if (nums.size() == 1) {
+            return nums[0];
+        } else if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        dp[1] = max(dp[0], nums[1]);
+        for (vector<int>::size_type i = 2; i < nums.size(); ++i) {
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+        }
+        return dp.back();
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
