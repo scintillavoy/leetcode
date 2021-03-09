80532e2a2b3127b063a97d3c0826903134973a3a
Moon Dongyoung <scintillavoy@gmail.com>
2021-03-09 22:24:24 +0900

    Solve 41

---
+++ 41_first-missing-positive.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for (vector<int>::size_type i = 0; i < nums.size(); ++i) {
+            while (0 < nums[i] && nums[i] <= nums.size() &&
+                    nums[i] != nums[nums[i]-1]) {
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        for (vector<int>::size_type i = 0; i < nums.size(); ++i) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+        return nums.size() + 1;
+    }
+};
+
+int main() {
+    Solution sol;
+    return 0;
+}
