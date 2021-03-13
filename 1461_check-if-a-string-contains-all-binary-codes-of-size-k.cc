91301c565d64cafa5b472b705c50ea803371fc96
Moon Dongyoung <scintillavoy@gmail.com>
2021-03-13 12:51:45 +0900

    Solve 1461

---
+++ 1461_check-if-a-string-contains-all-binary-codes-of-size-k.cc
@@ -0,0 +1,26 @@
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        unordered_set<string> check;
+        int count = 1 << k;
+        for (int i = 0; i+k <= s.size(); ++i) {
+            // Hashing can be improved by using rolling hash and
+            // the fact that s is a binary string
+            if (check.insert(s.substr(i, k)).second) {
+                --count;
+                if (count == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    return 0;
+}
