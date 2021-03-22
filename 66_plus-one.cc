f165e9f7412e80fbeceacbf4554b16f7fad5d074
Moon Dongyoung <scintillavoy@gmail.com>
2021-03-23 03:20:56 +0900

    Solve 66

---
+++ 66_plus-one.cc
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int idx = digits.size() - 1;
+        while (idx >= 0 && digits[idx] == 9) {
+            digits[idx] = 0;
+            --idx;
+        }
+        if (idx >= 0) {
+            ++digits[idx];
+        } else {
+            digits[0] = 1;
+            digits.push_back(0);
+        }
+        return digits;
+    }
+};
+
+int main() {
+    return 0;
+}
