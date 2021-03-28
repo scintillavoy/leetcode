c81a8b23514ec8da61d08e3bc5e6b35f915fb7df
Moon Dongyoung <scintillavoy@gmail.com>
2021-03-28 16:22:30 +0900

    Solve 1572

---
+++ 1572_matrix-diagonal-sum.cc
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum = 0;
+        for (int i = 0; i < mat.size(); ++i) {
+            sum += mat[i][i] + mat[i][mat.size()-i-1];
+        }
+        if (mat.size() % 2) {
+            sum -= mat[mat.size()/2][mat.size()/2];
+        }
+        return sum;
+    }
+};
+
+int main() {
+    return 0;
+}
