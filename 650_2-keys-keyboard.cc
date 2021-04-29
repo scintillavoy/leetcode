791df75ef7b911562aa6f4a5267c792a5845b671
Moon Dongyoung <scintillavoy@gmail.com>
2021-04-29 15:46:25 +0900

    Solve 650

---
+++ 650_2-keys-keyboard.cc
@@ -0,0 +1,18 @@
+class Solution {
+ public:
+  int minSteps(int n) {
+    int res = 0;
+    while (n > 1) {
+      for (int i = 2; i <= n; ++i) {
+        if (n % i == 0) {
+          res += i;
+          n /= i;
+          break;
+        }
+      }
+    }
+    return res;
+  }
+};
+
+int main() { return 0; }
