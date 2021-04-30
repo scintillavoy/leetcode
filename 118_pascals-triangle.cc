109df98c69e2a76464b3c30a2e9089f757bdd247
Moon Dongyoung <scintillavoy@gmail.com>
2021-04-30 12:24:10 +0900

    Solve 118

---
+++ 118_pascals-triangle.cc
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> res(numRows, vector<int>());
+    res[0].push_back(1);
+    for (int i = 1; i < numRows; ++i) {
+      res[i].push_back(1);
+      for (int j = 1; j < i; ++j) {
+        res[i].push_back(res[i-1][j-1] + res[i-1][j]);
+      }
+      res[i].push_back(1);
+    }
+    return res;
+  }
+};
+
+int main() { return 0; }
