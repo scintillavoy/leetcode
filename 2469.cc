dbf8eeab53e3aae037cddbb1c34d706df2d0fc4f
Moon Dongyoung <scintillavoy@gmail.com>
2024-03-18 09:51:39 +0900

    Add 2469.cc

---
+++ 2469.cc
@@ -0,0 +1,9 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<double> convertTemperature(double celsius) {
+    return vector<double>{celsius + 273.15, celsius * 1.80 + 32.00};
+  }
+};
