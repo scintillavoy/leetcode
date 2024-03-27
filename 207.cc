185301a4085abd4f6515709fc86e1f12f339ea6c
Moon Dongyoung <scintillavoy@gmail.com>
2024-03-28 02:30:13 +0900

    Add 207.cc

---
+++ 207.cc
@@ -0,0 +1,34 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> graph(numCourses);
+    vector<int> in_degree(numCourses);
+    for (const auto& p : prerequisites) {
+      graph[p[1]].push_back(p[0]);
+      ++in_degree[p[0]];
+    }
+    queue<int> q;
+    for (int i = 0; i < numCourses; ++i) {
+      if (in_degree[i] == 0) {
+        q.push(i);
+      }
+    }
+    vector<int> finished;
+    while (!q.empty()) {
+      int curr = q.front();
+      q.pop();
+      for (const auto& adj : graph[curr]) {
+        --in_degree[adj];
+        if (in_degree[adj] == 0) {
+          q.push(adj);
+        }
+      }
+      finished.push_back(curr);
+    }
+    return finished.size() == numCourses;
+  }
+};
