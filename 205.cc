a751ea875b2f75eae6bac930af461c5cdbd5b14e
Moon Dongyoung <scintillavoy@gmail.com>
2024-04-02 22:56:56 +0900

    Add 205.cc

---
+++ 205.cc
@@ -0,0 +1,19 @@
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+ public:
+  bool isIsomorphic(string s, string t) {
+    unordered_map<char, char> s_to_t, t_to_s;
+    for (int i = 0; i < s.size(); ++i) {
+      if ((s_to_t.count(s[i]) != 0 && s_to_t[s[i]] != t[i]) ||
+          (t_to_s.count(t[i]) != 0 && t_to_s[t[i]] != s[i])) {
+        return false;
+      }
+      s_to_t[s[i]] = t[i];
+      t_to_s[t[i]] = s[i];
+    }
+    return true;
+  }
+};
