46a5897d6749903b572f30513cc820e792f11fd2
Moon Dongyoung <scintillavoy@gmail.com>
2024-05-09 20:20:58 +0900

    Add 3075.cc

---
+++ 3075.cc
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  long long maximumHappinessSum(vector<int>& happiness, int k) {
+    sort(happiness.begin(), happiness.end(), greater<int>());
+    long long answer = 0;
+    for (int i = 0; i < k; ++i) {
+      answer += max(0, happiness[i] - i);
+    }
+    return answer;
+  }
+};
