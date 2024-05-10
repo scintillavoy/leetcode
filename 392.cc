40e89f2c41debec038dac55187947fa25348433e
Moon Dongyoung <scintillavoy@gmail.com>
2024-05-10 13:08:53 +0900

    Add 392.cc

---
+++ 392.cc
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  bool isSubsequence(string s, string t) {
+    int idx1 = 0, idx2 = 0;
+    while (idx1 < s.size() && idx2 < t.size()) {
+      if (s[idx1] == t[idx2]) {
+        ++idx1;
+      }
+      ++idx2;
+    }
+    return idx1 == s.size();
+  }
+};
