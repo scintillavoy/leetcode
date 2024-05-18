d1d3355238e5ecba5bb0e8be4bc3fd509afe2648
Moon Dongyoung <scintillavoy@gmail.com>
2024-05-18 20:51:36 +0900

    Add 1464.cc

---
+++ 1464.cc
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int maxProduct(vector<int>& nums) {
+    int max1 = 0, max2 = 0;
+    for (const auto num : nums) {
+      if (num > max1) {
+        max2 = max1;
+        max1 = num;
+      } else if (num > max2) {
+        max2 = num;
+      }
+    }
+    return (max1 - 1) * (max2 - 1);
+  }
+};
