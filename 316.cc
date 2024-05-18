cd4c64cf20f435dccb0b05e9a25fdc0c6150bbd9
Moon Dongyoung <scintillavoy@gmail.com>
2024-05-18 20:41:06 +0900

    Add 316.cc

---
+++ 316.cc
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  string removeDuplicateLetters(string s) {
+    vector<int> last_index(26, -1);
+    for (int i = 0; i < s.size(); ++i) {
+      last_index[s[i] - 'a'] = i;
+    }
+    vector<bool> exist(26);
+    string result;
+    for (int i = 0; i < s.size(); ++i) {
+      if (exist[s[i] - 'a']) {
+        continue;
+      }
+      while (!result.empty() && result.back() >= s[i] &&
+             i <= last_index[result.back() - 'a']) {
+        exist[result.back() - 'a'] = false;
+        result.pop_back();
+      }
+      exist[s[i] - 'a'] = true;
+      result.push_back(s[i]);
+    }
+    return result;
+  }
+};
