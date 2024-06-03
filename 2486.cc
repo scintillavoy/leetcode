59c11f91f9bd81b95aca0aed8f34e2a2b4e772da
Moon Dongyoung <scintillavoy@gmail.com>
2024-06-03 14:36:53 +0900

    Add 2486.cc

---
+++ 2486.cc
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  int appendCharacters(string s, string t) {
+    int i = 0, j = 0;
+    while (i < s.size() && j < t.size()) {
+      if (s[i] == t[j]) {
+        ++j;
+      }
+      ++i;
+    }
+    return t.size() - j;
+  }
+};
