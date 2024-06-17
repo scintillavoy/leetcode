07f0e1962599ff2f16eb0bd2c937681243416cad
Moon Dongyoung <scintillavoy@gmail.com>
2024-06-17 18:19:42 +0900

    Add 767.cc

---
+++ 767.cc
@@ -0,0 +1,44 @@
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  string reorganizeString(string s) {
+    vector<int> counts(26);
+    for (const auto c : s) {
+      ++counts[c - 'a'];
+    }
+    priority_queue<pair<int, int>> pq;
+    for (int i = 0; i < counts.size(); ++i) {
+      if (counts[i] > 0) {
+        pq.push({counts[i], i + 'a'});
+      }
+    }
+    string result;
+    while (!pq.empty()) {
+      auto [count, ch] = pq.top();
+      pq.pop();
+      if (result.empty() || ch != result.back()) {
+        result += ch;
+        if (count >= 2) {
+          pq.push({count - 1, ch});
+        }
+      } else {
+        if (pq.empty()) {
+          return "";
+        }
+        auto [count2, ch2] = pq.top();
+        pq.pop();
+        result += ch2;
+        if (count2 >= 2) {
+          pq.push({count2 - 1, ch2});
+        }
+        pq.push({count, ch});
+      }
+    }
+    return result;
+  }
+};
