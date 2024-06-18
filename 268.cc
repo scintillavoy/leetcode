9670f7a76566a779622305fc9417b00602cff5e9
Moon Dongyoung <scintillavoy@gmail.com>
2024-06-18 20:20:58 +0900

    Add 268.cc

---
+++ 268.cc
@@ -0,0 +1,21 @@
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int missingNumber(vector<int>& nums) {
+    nums.push_back(-1);
+    for (int i = 0; i < nums.size(); ++i) {
+      while (nums[i] != i && nums[i] != -1) {
+        swap(nums[i], nums[nums[i]]);
+      }
+    }
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i] == -1) {
+        return i;
+      }
+    }
+    return -1;
+  }
+};
