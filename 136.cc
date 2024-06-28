ae3a45fc77032672fc2dc773122b4410501339e7
Moon Dongyoung <scintillavoy@gmail.com>
2024-06-28 17:42:15 +0900

    Add 136.cc

---
+++ 136.cc
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int singleNumber(vector<int>& nums) {
+    int result = 0;
+    for (const auto num : nums) {
+      result ^= num;
+    }
+    return result;
+  }
+};
