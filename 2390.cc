ee4f5a1c7de40330d1fb4373e398b40b6eb27d3f
Moon Dongyoung <scintillavoy@gmail.com>
2024-07-01 05:54:03 +0900

    Add 2390.cc

---
+++ 2390.cc
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  string removeStars(string s) {
+    string result;
+    for (const auto c : s) {
+      if (c == '*') {
+        result.pop_back();
+      } else {
+        result.push_back(c);
+      }
+    }
+    return result;
+  }
+};
