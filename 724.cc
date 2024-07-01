235f3f1116b76c10ffc9ab08f0287d77fe90afbb
Moon Dongyoung <scintillavoy@gmail.com>
2024-07-01 18:01:39 +0900

    Add 724.cc

---
+++ 724.cc
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int pivotIndex(vector<int>& nums) {
+    int left_sum = 0;
+    int right_sum = 0;
+    for (const auto num : nums) {
+      right_sum += num;
+    }
+    right_sum -= nums.front();
+    if (left_sum == right_sum) {
+      return 0;
+    }
+    for (int i = 1; i < nums.size(); ++i) {
+      left_sum += nums[i - 1];
+      right_sum -= nums[i];
+      if (left_sum == right_sum) {
+        return i;
+      }
+    }
+    return -1;
+  }
+};
