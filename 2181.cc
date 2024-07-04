cb983a340d0e250df319a8c2e4e2604bdb9ee0f1
Moon Dongyoung <scintillavoy@gmail.com>
2024-07-04 20:38:02 +0900

    Add 2181.cc

---
+++ 2181.cc
@@ -0,0 +1,37 @@
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* mergeNodes(ListNode* head) {
+    ListNode* curr = head;
+    while (curr->next != nullptr) {
+      if (curr->next->val == 0) {
+        if (curr->next->next == nullptr) {
+          curr->next = nullptr;
+        } else {
+          curr = curr->next;
+        }
+      } else {
+        curr->val += curr->next->val;
+        curr->next = curr->next->next;
+      }
+    }
+    return head;
+  }
+};
