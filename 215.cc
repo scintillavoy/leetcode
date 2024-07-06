4d81f490d3cf948312b30e867da162122604c292
Moon Dongyoung <scintillavoy@gmail.com>
2024-07-06 16:32:08 +0900

    Add 215.cc

---
+++ 215.cc
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <functional>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int findKthLargest(vector<int>& nums, int k) {
+    nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
+    return nums[k - 1];
+  }
+};
