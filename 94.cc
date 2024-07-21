b2ddc8b9742a7cd48948b7c6456ddfc44273e3ca
Moon Dongyoung <scintillavoy@gmail.com>
2024-07-21 15:57:56 +0900

    Add 94.cc

---
+++ 94.cc
@@ -0,0 +1,43 @@
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> result;
+    traverse(root, result);
+    return result;
+  }
+
+ private:
+  void traverse(TreeNode *node, vector<int> &result) {
+    if (node == nullptr) {
+      return;
+    }
+    traverse(node->left, result);
+    result.push_back(node->val);
+    traverse(node->right, result);
+  }
+};
