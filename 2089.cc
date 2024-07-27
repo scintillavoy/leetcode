d4f7ec81bfc800dca4263fbe38a961254dd13823
Moon Dongyoung <scintillavoy@gmail.com>
2024-07-28 01:35:08 +0900

    Add 2089.cc

---
+++ 2089.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> targetIndices(vector<int>& nums, int target) {
+    sort(nums.begin(), nums.end());
+    vector<int> result;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i] == target) {
+        result.push_back(i);
+      }
+    }
+    return result;
+  }
+};
