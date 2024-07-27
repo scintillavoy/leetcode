eae55984b38645b4c6f1dff6288a52138d7f3051
Moon Dongyoung <scintillavoy@gmail.com>
2024-07-28 01:47:03 +0900

    Add 3151.cc

---
+++ 3151.cc
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  bool isArraySpecial(vector<int>& nums) {
+    for (int i = 1; i < nums.size(); ++i) {
+      if (((nums[i - 1] ^ nums[i]) & 1) == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
