ac3c9be54a30b9d438613dc83efa90dcdd7f0796
Dongyoung Moon <scintillavoy@gmail.com>
2024-08-13 21:35:21 +0900

    Add 19.cc

---
+++ 19.cc
@@ -0,0 +1,38 @@
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  ListNode* removeNthFromEnd(ListNode* head, int n) {
+    int length = 0;
+    ListNode* curr = head;
+    while (curr != nullptr) {
+      curr = curr->next;
+      ++length;
+    }
+    if (n == length) {
+      return head->next;
+    }
+    curr = head;
+    for (int i = 0; i < length - n - 1; ++i) {
+      curr = curr->next;
+    }
+    curr->next = curr->next->next;
+    return head;
+  }
+};
