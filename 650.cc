484594a4918955215585ff776e7ffe0e8db96ce8
Dongyoung Moon <scintillavoy@gmail.com>
2024-08-19 10:15:35 +0900

    Add 650.cc

---
+++ 650.cc
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <limits>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int minSteps(int n) {
+    vector<int> dp(n + 1, numeric_limits<int>::max());
+    dp[1] = 0;
+    for (int i = 2; i <= n; ++i) {
+      for (int j = 1; j <= i / 2; ++j) {
+        if (i % j == 0) {
+          dp[i] = min(dp[i], dp[j] + i / j);
+        }
+      }
+    }
+    return dp[n];
+  }
+};
