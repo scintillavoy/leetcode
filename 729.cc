9dff0579c4a77821a896b3004b5c1740c961d870
Dongyoung Moon <scintillavoy@gmail.com>
2024-09-26 18:14:53 +0900

    Add 729.cc

---
+++ 729.cc
@@ -0,0 +1,31 @@
+#include <map>
+using namespace std;
+
+class MyCalendar {
+ public:
+  MyCalendar() {}
+
+  bool book(int start, int end) {
+    auto it = events.lower_bound(start);
+    if (it != events.end() && it->first < end) {
+      return false;
+    }
+    if (it != events.begin()) {
+      --it;
+      if (it->second > start) {
+        return false;
+      }
+    }
+    events[start] = end;
+    return true;
+  }
+
+ private:
+  map<int, int> events;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
