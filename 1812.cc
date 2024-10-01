df9e9058a9cdc953951f014a3dd67c93bfe66eba
Dongyoung Moon <scintillavoy@gmail.com>
2024-10-01 18:07:48 +0900

    Add 1812.cc

---
+++ 1812.cc
@@ -0,0 +1,9 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  bool squareIsWhite(string coordinates) {
+    return (coordinates[0] - 'a' + coordinates[1] - '0') % 2 == 0;
+  }
+};
