9b855d804db2fc4a3066a5c732cd13dbaebc042c
Dongyoung Moon <scintillavoy@gmail.com>
2024-12-09 17:07:37 +0900

    Add 387.cc

---
+++ 387.cc
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int firstUniqChar(string s) {
+    vector<int> counts(26);
+    for (int i = 0; i < s.size(); ++i) {
+      ++counts[s[i] - 'a'];
+    }
+    for (int i = 0; i < s.size(); ++i) {
+      if (counts[s[i] - 'a'] == 1) {
+        return i;
+      }
+    }
+    return -1;
+  }
+};
