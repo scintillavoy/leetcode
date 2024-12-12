1d69a5357241f8aea477b6b6dfb5ecc92b3bbd8b
Dongyoung Moon <scintillavoy@gmail.com>
2024-12-12 17:30:18 +0900

    Add 167.cc

---
+++ 167.cc
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& numbers, int target) {
+    int left = 0, right = numbers.size() - 1;
+    while (numbers[left] + numbers[right] != target) {
+      if (numbers[left] + numbers[right] > target) {
+        --right;
+      } else {
+        ++left;
+      }
+    }
+    return {left + 1, right + 1};
+  }
+};
