5e7bfec71d19a5dcd6332fe82fdb0336ab3a2722
Dongyoung Moon <scintillavoy@gmail.com>
2025-01-08 18:23:33 +0900

    Add 3045.cc

---
+++ 3045.cc
@@ -0,0 +1,55 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Trie {
+ public:
+  Trie() { root = new TrieNode(); }
+  ~Trie() { delete_trie(root); }
+
+  int insert(const string &word) {
+    TrieNode *curr = root;
+    int total_count = 0;
+    for (int i = 0; i < word.size(); ++i) {
+      int key = (word[i] - 'a') * 26 + word[word.size() - 1 - i] - 'a';
+      if (!curr->children.contains(key)) {
+        curr->children[key] = new TrieNode();
+      }
+      curr = curr->children[key];
+      total_count += curr->count;
+    }
+    curr->count += 1;
+    return total_count;
+  }
+
+ private:
+  struct TrieNode {
+    int count = 0;
+    unordered_map<int, TrieNode *> children;
+  };
+
+  TrieNode *root;
+
+  void delete_trie(TrieNode *node) {
+    if (node == nullptr) {
+      return;
+    }
+    for (auto it = node->children.begin(); it != node->children.end(); ++it) {
+      delete_trie(it->second);
+    }
+    delete node;
+  }
+};
+
+class Solution {
+ public:
+  long long countPrefixSuffixPairs(vector<string> &words) {
+    Trie trie;
+    long long result = 0;
+    for (const auto &word : words) {
+      result += trie.insert(word);
+    }
+    return result;
+  }
+};
