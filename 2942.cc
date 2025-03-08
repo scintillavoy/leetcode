41e7c66189a52fed29fc2ee2a788e1574a038ae1
Dongyoung Moon <scintillavoy@gmail.com>
2025-03-09 03:10:40 +0900

    Add 2942.cc

---
+++ 2942.cc
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> findWordsContaining(vector<string>& words, char x) {
+    vector<int> result;
+    for (int i = 0; i < words.size(); ++i) {
+      if (words[i].find(x) != string::npos) {
+        result.push_back(i);
+      }
+    }
+    return result;
+  }
+};
