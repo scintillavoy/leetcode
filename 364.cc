43b5ba44cbc51da34cfab800e5afecef3fa7be67
Dongyoung Moon <scintillavoy@gmail.com>
2025-03-15 15:36:20 +0900

    Add 364.cc

---
+++ 364.cc
@@ -0,0 +1,91 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+class NestedInteger {
+ public:
+  // Constructor initializes an empty nested list.
+  NestedInteger();
+
+  // Constructor initializes a single integer.
+  NestedInteger(int value);
+
+  // Return true if this NestedInteger holds a single integer, rather than a
+  // nested list.
+  bool isInteger() const;
+
+  // Return the single integer that this NestedInteger holds, if it holds a
+  // single integer
+  // The result is undefined if this NestedInteger holds a nested list
+  int getInteger() const;
+
+  // Set this NestedInteger to hold a single integer.
+  void setInteger(int value);
+
+  // Set this NestedInteger to hold a nested list and adds a nested integer to
+  // it.
+  void add(const NestedInteger &ni);
+
+  // Return the nested list that this NestedInteger holds, if it holds a nested
+  // list
+  // The result is undefined if this NestedInteger holds a single integer
+  const vector<NestedInteger> &getList() const;
+};
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than
+ * a nested list. bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a
+ * single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer
+ * to it. void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a
+ * nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+ public:
+  int depthSumInverse(vector<NestedInteger> &nestedList) {
+    int total_sum = 0;
+    int unique_sum = 0;
+    queue<vector<NestedInteger>> q;
+    q.push(nestedList);
+    while (!q.empty()) {
+      for (int i = q.size(); i > 0; --i) {
+        vector<NestedInteger> nested_list = q.front();
+        q.pop();
+        for (const auto &nested_integer : nested_list) {
+          if (nested_integer.isInteger()) {
+            unique_sum += nested_integer.getInteger();
+          } else {
+            q.push(nested_integer.getList());
+          }
+        }
+      }
+      total_sum += unique_sum;
+    }
+    return total_sum;
+  }
+};
