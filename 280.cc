61854bcbe9df111a0b59c2de7ba244f69b22d0a2
Dongyoung Moon <scintillavoy@gmail.com>
2025-03-21 20:18:55 +0900

    Add 280.cc

---
+++ 280.cc
@@ -0,0 +1,20 @@
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  void wiggleSort(vector<int>& nums) {
+    for (int i = 1; i < nums.size(); ++i) {
+      if (i % 2 == 0) {
+        if (nums[i - 1] < nums[i]) {
+          swap(nums[i - 1], nums[i]);
+        }
+      } else {
+        if (nums[i - 1] > nums[i]) {
+          swap(nums[i - 1], nums[i]);
+        }
+      }
+    }
+  }
+};
