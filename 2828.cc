6d9e1e55a0d7023d76618434f7f2caf4852339c9
Dongyoung Moon <scintillavoy@gmail.com>
2025-03-22 20:32:15 +0900

    Add 2828.cc

---
+++ 2828.cc
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  bool isAcronym(vector<string>& words, string s) {
+    if (words.size() != s.size()) {
+      return false;
+    }
+    for (int i = 0; i < words.size(); ++i) {
+      if (words[i][0] != s[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
