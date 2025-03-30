dc297c07fb60d9cbaa39dbe62f9c677b657f3786
Dongyoung Moon <scintillavoy@gmail.com>
2025-03-30 19:00:22 +0900

    Add 3461.cc

---
+++ 3461.cc
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  bool hasSameDigits(string s) {
+    while (s.size() > 2) {
+      for (int i = 1; i < s.size(); ++i) {
+        s[i - 1] = (s[i - 1] - '0' + s[i] - '0') % 10 + '0';
+      }
+      s.pop_back();
+    }
+    return s[0] == s[1];
+  }
+};
