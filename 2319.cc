87c152c3f4632973bf375761e6b5e1cdc9a9388d
Dongyoung Moon <scintillavoy@gmail.com>
2025-04-04 16:20:22 +0900

    Add 2319.cc

---
+++ 2319.cc
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  bool checkXMatrix(vector<vector<int>>& grid) {
+    int n = grid.size();
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (i == j || i + j + 1 == n) {
+          if (grid[i][j] == 0) {
+            return false;
+          }
+        } else {
+          if (grid[i][j] != 0) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+};
