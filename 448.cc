63ee5cb35235ed996df81e40e3fc42b7d925a093
Dongyoung Moon <scintillavoy@gmail.com>
2025-04-09 15:35:18 +0900

    Add 448.cc

---
+++ 448.cc
@@ -0,0 +1,21 @@
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> findDisappearedNumbers(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); ++i) {
+      while (nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
+        swap(nums[i], nums[nums[i] - 1]);
+      }
+    }
+    vector<int> result;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (nums[i] != i + 1) {
+        result.push_back(i + 1);
+      }
+    }
+    return result;
+  }
+};
