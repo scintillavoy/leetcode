fdc38400f2d4d422a2be60a38e1f076678b2417f
Dongyoung Moon <scintillavoy@gmail.com>
2025-04-14 14:07:11 +0900

    Add 1534.cc

---
+++ 1534.cc
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+    int count = 0;
+    for (int i = 0; i < arr.size(); ++i) {
+      for (int j = i + 1; j < arr.size(); ++j) {
+        if (abs(arr[i] - arr[j]) > a) {
+          continue;
+        }
+        for (int k = j + 1; k < arr.size(); ++k) {
+          if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
+            ++count;
+          }
+        }
+      }
+    }
+    return count;
+  }
+};
