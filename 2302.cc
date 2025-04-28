9030e99331c53dc2595bac7ccd0f64b25076559a
Dongyoung Moon <scintillavoy@gmail.com>
2025-04-28 11:44:08 +0900

    Add 2302.cc

---
+++ 2302.cc
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  long long countSubarrays(vector<int>& nums, long long k) {
+    long long result = 0;
+    int start = 0;
+    long long sum = 0;
+    for (int end = 0; end < nums.size(); ++end) {
+      sum += nums[end];
+      while (sum * (end - start + 1) >= k) {
+        sum -= nums[start];
+        ++start;
+      }
+      result += end - start + 1;
+    }
+    return result;
+  }
+};
