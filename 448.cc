371b6b5d687825def23d24f027ad5e417f678305
Dongyoung Moon <scintillavoy@gmail.com>
2026-05-21 20:20:55 +0900

    Update 448.cc

--- 448.cc
+++ 448.cc
@@ -6,7 +6,7 @@ class Solution {
  public:
   vector<int> findDisappearedNumbers(vector<int>& nums) {
     for (int i = 0; i < nums.size(); ++i) {
-      while (nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
+      while (nums[i] != nums[nums[i] - 1]) {
         swap(nums[i], nums[nums[i] - 1]);
       }
     }
