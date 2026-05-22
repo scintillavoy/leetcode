dcfeff4de39b87089d06942e86a9a8eadcf2e162
Dongyoung Moon <scintillavoy@gmail.com>
2026-05-22 20:43:02 +0900

    Add 459.cc

---
+++ 459.cc
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  bool repeatedSubstringPattern(string s) {
+    string doubled = s + s;
+    string sub = doubled.substr(1, doubled.size() - 2);
+    return sub.find(s) != string::npos;
+  }
+};
