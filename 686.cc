d2e7804beaf8bef28546d6bc5f0942273ae88670
Dongyoung Moon <scintillavoy@gmail.com>
2026-05-22 22:03:09 +0900

    Add 686.cc

---
+++ 686.cc
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  int repeatedStringMatch(string a, string b) {
+    string repeated(a);
+    int count = 1;
+    while (repeated.size() < b.size()) {
+      repeated += a;
+      ++count;
+    }
+    if (repeated.find(b) != string::npos) {
+      return count;
+    }
+    repeated += a;
+    ++count;
+    if (repeated.find(b) != string::npos) {
+      return count;
+    }
+    return -1;
+  }
+};
