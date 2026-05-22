418c6f4c35e84523191198e1446c16307bde86c9
Dongyoung Moon <scintillavoy@gmail.com>
2026-05-22 23:48:22 +0900

    Add 138.cc

---
+++ 138.cc
@@ -0,0 +1,55 @@
+#include <unordered_map>
+using namespace std;
+
+#define NULL nullptr
+
+class Node {
+ public:
+  int val;
+  Node* next;
+  Node* random;
+
+  Node(int _val) {
+    val = _val;
+    next = NULL;
+    random = NULL;
+  }
+};
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+ public:
+  Node* copyRandomList(Node* head) {
+    if (head == nullptr) {
+      return nullptr;
+    }
+    unordered_map<Node*, Node*> old_to_new;
+    Node* curr = head;
+    while (curr != nullptr) {
+      old_to_new[curr] = new Node(curr->val);
+      curr = curr->next;
+    }
+    curr = head;
+    while (curr != nullptr) {
+      old_to_new[curr]->next = old_to_new[curr->next];
+      old_to_new[curr]->random = old_to_new[curr->random];
+      curr = curr->next;
+    }
+    return old_to_new[head];
+  }
+};
