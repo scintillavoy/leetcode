9b7a6e5088a8527eb44d7b91bfba40df36e56ec0
Dongyoung Moon <scintillavoy@gmail.com>
2026-05-31 20:43:41 +0900

    Add 100.cc

---
+++ 100.cc
@@ -0,0 +1,43 @@
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+ public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (p == nullptr && q == nullptr) {
+      return true;
+    }
+    if (p == nullptr) {
+      return false;
+    }
+    if (q == nullptr) {
+      return false;
+    }
+    if (!isSameTree(p->left, q->left)) {
+      return false;
+    }
+    if (!isSameTree(p->right, q->right)) {
+      return false;
+    }
+    return p->val == q->val;
+  }
+};
