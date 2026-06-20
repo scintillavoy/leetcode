c3cf07508b2afca43be42bb3eb734fa80c42f8f0
Dongyoung Moon <scintillavoy@gmail.com>
2026-06-20 23:28:46 +0900

    Add 1876.cc

---
+++ 1876.cc
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  int countGoodSubstrings(string s) {
+    int count = 0;
+    for (int i = 2; i < s.size(); ++i) {
+      if (s[i - 2] != s[i - 1] && s[i - 1] != s[i] && s[i] != s[i - 2]) {
+        ++count;
+      }
+    }
+    return count;
+  }
+};
