0fb0811d64b2d36545ba0552a39c96bc968c41ee
Dongyoung Moon <scintillavoy@gmail.com>
2026-06-20 22:38:04 +0900

    Add 1903.cc

---
+++ 1903.cc
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  string largestOddNumber(string num) {
+    while (!num.empty() && (num.back() - '0') % 2 == 0) {
+      num.pop_back();
+    }
+    return num;
+  }
+};
