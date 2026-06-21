b8648b36fe665797e1cefc93178a002ef8300066
Dongyoung Moon <scintillavoy@gmail.com>
2026-06-21 17:54:30 +0900

    Add 867.cc

---
+++ 867.cc
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    vector<vector<int>> transposed(n, vector<int>(m));
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        transposed[j][i] = matrix[i][j];
+      }
+    }
+    return transposed;
+  }
+};
