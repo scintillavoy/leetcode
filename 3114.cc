970a6b8a7afbb24956188209708fc0534ddd3aac
Dongyoung Moon <scintillavoy@gmail.com>
2026-06-23 22:43:02 +0900

    Add 3114.cc

---
+++ 3114.cc
@@ -0,0 +1,29 @@
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  string findLatestTime(string s) {
+    if (s[0] == '?') {
+      if ('2' <= s[1] && s[1] <= '9') {
+        s[0] = '0';
+      } else {
+        s[0] = '1';
+      }
+    }
+    if (s[1] == '?') {
+      if (s[0] == '1') {
+        s[1] = '1';
+      } else {
+        s[1] = '9';
+      }
+    }
+    if (s[3] == '?') {
+      s[3] = '5';
+    }
+    if (s[4] == '?') {
+      s[4] = '9';
+    }
+    return s;
+  }
+};
